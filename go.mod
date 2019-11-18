dd8e993f41a8730952998da253fb167b6e68fa74
Jacob <jsmoody@iastate.edu>
2019-11-18 03:54:12 -0600

    Add auth support through aitm

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.13
 
 require (
 	github.com/gomarkdown/markdown v0.0.0-20191104174740-4d42851d4d5a
+	github.com/majiru/aitm v0.0.0-20191118092908-a094cfc384b3
 	github.com/microcosm-cc/bluemonday v1.0.2
 )
--- main.go
+++ main.go
@@ -5,20 +5,42 @@ import (
 	"net/http"
 	"os"
 	"flag"
+
+	"github.com/majiru/aitm"
 )
 
 const frontPage = "FrontPage"
 const dataDir = "./data"
 
 var port string
+var auth string
 
 func init() {
 	flag.StringVar(&port, "port", "8080", "port to run webserver on")
+	flag.StringVar(&auth, "auth", "", "config file for aitm")
 	os.Mkdir(dataDir, 0755)
 }
 
 func main() {
 	flag.Parse()
-	http.HandleFunc("/", rootHandler)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	mux := &http.ServeMux{}
+	mux.HandleFunc("/", rootHandler)
+	srv := &http.Server{
+		Addr: ":"+port,
+		Handler: mux,
+	}
+	if auth == "" {
+		log.Fatal(srv.ListenAndServe())
+	}
+	asrv := aitm.WrapServer(srv)
+	f, err := os.Open(auth)
+	if err != nil {
+		log.Fatalf("could not open %s: %v\n", auth, err)
+	}
+	err = asrv.LoadUsers(f)
+	if err != nil {
+		log.Fatal("err parsing users conf:", err)
+	}
+	f.Close()
+	log.Fatal(asrv.ListenAndServe())
 }
